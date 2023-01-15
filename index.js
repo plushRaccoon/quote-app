c307bf00ef99b5042b8fe9f0b797d8f21cba5f21
Iryna Pliakhanava <irkapuh@gmail.com>
2023-01-15 23:43:20 +0100

    feat: add an opportunity to get quote

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+"use strict";
 
+const quote = document.querySelector(".quote"),
+  author = document.querySelector(".author"),
+  newQuote = document.querySelector('.button_generate'),
+  addQuote = document.querySelector('.button_add'),
+  delQuote = document.querySelector('.button_delete');
+
+
+let URL = "https://api.quotable.io/random";
+
+async function getQuote(url) {
+  let response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Error in ${url} status: ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+getQuote(URL)
+  .then(data => {
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+newQuote.addEventListener('click', () => {
+  getQuote(URL)
+  .then(data => {
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
