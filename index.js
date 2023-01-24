4cd053a5935b913a52d26294f665603a45130607
Iryna Pliakhanava <irkapuh@gmail.com>
2023-01-24 23:22:19 +0100

    feat: add an opportunity to delete items from list and store

--- index.js
+++ index.js
@@ -1,117 +1,147 @@
 "use strict";
 
-const quote = document.querySelector(".quote"),
-  author = document.querySelector(".author"),
-  buttons = document.querySelectorAll(".button"),
-  quoList = document.querySelector(".citate-list__list");
-  // let db;
-let URL = "https://api.quotable.io/random";
+document.addEventListener("DOMContentLoaded", () => {
+  const quote = document.querySelector(".quote"),
+    author = document.querySelector(".author"),
+    buttons = document.querySelectorAll(".button"),
+    quoList = document.querySelector(".citate-list__list");
 
-async function getQuote(url) {
-  let response = await fetch(url);
+  let db;
+   openDB();
+  let URL = "https://api.quotable.io/random";
 
-  if (!response.ok) {
-    throw new Error(`Error in ${url} status: ${response.status}`);
+  async function getQuote(url) {
+    let response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error in ${url} status: ${response.status}`);
+    }
+
+    return await response.json();
   }
 
-  return await response.json();
-}
+  function generateContent(func) {
+    return func
+      .then((data) => {
+        quote.innerHTML = data.content;
+        author.innerHTML = data.author;
+        quote.dataset.id = `${data._id}`;
+        author.dataset.id = `${data._id}`;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-function generateContent(func) {
-  return func
-    .then((data) => {
-      quote.innerHTML = data.content;
-      author.innerHTML = data.author;
-      quote.dataset.id = `${data._id}`;
-      author.dataset.id = `${data._id}`;
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+  generateContent(getQuote(URL));
+  // addToList();
 
-generateContent(getQuote(URL));
-
-function addToList() {
-  let qouteItem;
-  let quoText = quote.innerHTML;
-  qouteItem = `
-        <li class="list__item">
-          <p class="item__text" data-quote-id='${quote.dataset.id}'>
-            ${quoText} ${author.innerHTML}
-          </p>
-          <button class="item__button button button_delete">
-            <img src="assets/svg/bin.svg" alt="delete" />
-          </button>
-        </li>
-      `;
+  function addToList(data) {
+    data.forEach(item => {
+      let qouteItem = `
+      <li class="list__item">
+        <p class="item__text" data-quote-id='${item.id}'>
+          ${item.quote} ${item.author}
+        </p>
+        <button class="item__button button button_delete" data-button-id='${item.id}'>
+          <img src="assets/svg/bin.svg" alt="delete" />
+        </button>
+      </li>
+    `;
   quoList.insertAdjacentHTML("beforeend", qouteItem);
-  let db;
-  let openDB = indexedDB.open("quotesDB", 2);
+    });
+  }
 
-      openDB.onerror = (e) => {
-        console.error(e);
+  function openDB() {
+    let openDB = indexedDB.open("quotesDB", 2);
+
+    openDB.onerror = (e) => {
+      console.error(e);
+    };
+
+    openDB.onsuccess = (e) => {
+      db = openDB.result;
+      
+      addData();
+      getDataFromStore();
+      console.log("DB opened successfully");
+    };
+
+    openDB.onupgradeneeded = (e) => {
+      db = openDB.result;
+
+      db.onerror = (e) => {
+        console.log("Error in initializing DB");
       };
 
-  openDB.onsuccess = (e) => {
-    
-        db = openDB.result;
-        addData(db, quote.dataset.id);
-        console.log("DB opened successfully");
-      };
+      if (!db.objectStoreNames.contains("quotes")) {
+        const quotesOS = db.createObjectStore("quotes", {
+          autoIncrement: true,
+        });
+      }
 
-      openDB.onupgradeneeded = (e) => {
-        db = openDB.result;
+      console.log("Object store created.");
+    };
+  }
 
-        db.onerror = (e) => {
-          console.log("Error in initializing DB");
+  function deleteItem(del) {
+    // del.forEach((btnDel) => {
+    //   btnDel.addEventListener("click", () => {
+        let tx = db.transaction(['quotes'], 'readwrite');
+        let store = tx.objectStore('quotes');
+        let req = store.delete(del.dataset.buttonId);
+        req.onsuccess = () => {
+          console.log('deleted');
         };
+        del.parentElement.remove();
+    //   });
+    // });
+  }
 
-        if (!db.objectStoreNames.contains("quotes")) {
-          const quotesOS = db.createObjectStore("quotes", { keyPath: "id", autoIncrement: true });
-        }
 
-        console.log("Object store created.");
-      };
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      let btn = e.target;
 
-}
+      if (btn.classList.contains("button_generate")) {
+        generateContent(getQuote(URL));
+      }
 
-function deleteItem(del) {
-  del.forEach((btnDel) => {
-    btnDel.addEventListener("click", () => {
-      btnDel.parentElement.remove();
+      if (btn.classList.contains("button_add")) {
+       
+        
+
+        let btnsDel = quoList.querySelectorAll(".button_delete");
+        console.log(btnsDel);
+        deleteItem(btn);
+      }
     });
   });
-}
 
+  function addData() {
+    let item = [{ quote: quote.innerHTML, author: author.innerHTML }];
+    let tx = db.transaction(["quotes"], "readwrite");
+    let store = tx.objectStore("quotes");
+    // console.log(id);
+    let req = store.put(item[0]);
 
+    req.onsuccess = () => {
+      console.log("we did it");
+    };
+  }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    let btn = e.target;
+  function getDataFromStore() {
+    let tx = db.transaction(["quotes"], "readonly");
+    let store = tx.objectStore("quotes");
 
-    if (btn.classList.contains("button_generate")) {
-      generateContent(getQuote(URL));
-    }
+    let req = store.getAll();
+    req.onsuccess = () => {
+      console.log(req.result);
+      addToList(req.result);
+    };
+  }
 
-    if (btn.classList.contains("button_add")) {
-      addToList();
+  
 
-      let btnsDel = document.querySelectorAll(".button_delete");
-      deleteItem(btnsDel);
-    }
-  });
 });
 
-function addData(db, id) {
-  let item = [{ id, quote: quote.innerHTML, author: author.innerHTML }];
-  let tx = db.transaction(["quotes"], "readwrite");
-  let store = tx.objectStore("quotes");
-  console.log(id);
-  let req = store.put(item[0]);
-
-  req.onsuccess = () => {
-
-    console.log("we did it", req.result);
-  };
-}
